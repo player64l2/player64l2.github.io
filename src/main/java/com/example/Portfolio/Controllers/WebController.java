09a538327a456d691835a7017b9883a6f93d941d
player64l <leonardoxdgames@gmail.com>
2026-03-10 00:26:02 -0300

    Upload de Projeto java e Spring boot para testes de CI/CD e github Actions

---
+++ src/main/java/com/example/Portfolio/Controllers/WebController.java
@@ -0,0 +1,15 @@
+package com.example.Portfolio.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/index")
+	public String index(){
+	    return "index";
+	}
+
+	
+}
